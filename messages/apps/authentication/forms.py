a9bcc8c9993e346d9eacd5eba812b54f5f70bb99
Spookywy <v-menoret@outlook.com>
2022-11-04 16:29:32 +0100

    :sparkles: simplify auth views

--- messages/apps/authentication/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class LoginForm(forms.Form):
-    username = forms.CharField(max_length=20, label='Username')
-    password = forms.CharField(max_length=25, widget=forms.PasswordInput, label="Password")
--- messages/apps/authentication/urls.py
+++ messages/apps/authentication/urls.py
@@ -1,10 +1,22 @@
 from django.urls import path
+from django.contrib.auth.views import (
+    LoginView,
+    LogoutView,
+    PasswordChangeView,
+    PasswordChangeDoneView
+)
+
 
 
 from . import views
 
 
 urlpatterns = [
-    path('', views.LoginPageView.as_view(), name='login'),
-    path('logout', views.logout_user, name='logout')
+    path('', LoginView.as_view(
+            template_name='authentication/login.html',
+            redirect_authenticated_user=True),
+        name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('change-password/', PasswordChangeView.as_view(), name='change_password'),
+    path('password-change-done/', PasswordChangeDoneView.as_view(), name='password_change_done')
 ]
