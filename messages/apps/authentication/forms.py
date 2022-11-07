c4e2c1a7662bf083cbcc880e417f08316683b87d
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2022-11-07 22:59:54 +0100

    :lipstick: update signup UI page

--- messages/apps/authentication/forms.py
+++ messages/apps/authentication/forms.py
@@ -10,13 +10,16 @@ from django import forms
 class UserLoginForm(AuthenticationForm):
     username = UsernameField(
         widget=forms.TextInput(
-            attrs={"class": "form-control", "placeholder": "Enter your username"}
+            attrs={
+                "class": "form-control block-form-control",
+                "placeholder": "Enter your username",
+            }
         )
     )
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "class": "form-control",
+                "class": "form-control block-form-control",
                 "placeholder": "Enter your password",
             }
         )
@@ -27,3 +30,44 @@ class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = get_user_model()
         fields = ("username", "first_name", "last_name")
+
+    username = UsernameField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control block-form-control",
+                "placeholder": "Username",
+            }
+        )
+    )
+    first_name = UsernameField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control inline-form-control",
+                "placeholder": "First Name",
+            }
+        )
+    )
+    last_name = UsernameField(
+        widget=forms.TextInput(
+            attrs={
+                "class": "form-control inline-form-control",
+                "placeholder": "Last Name",
+            }
+        )
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control block-form-control",
+                "placeholder": "Password",
+            }
+        )
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control block-form-control",
+                "placeholder": "Confirm password",
+            }
+        )
+    )
