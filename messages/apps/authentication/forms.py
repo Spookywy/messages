1bc4fef91a59623f1d5cfe08826436a611a5ba97
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2022-11-07 21:05:36 +0100

    :lipstick: update login page style

--- messages/apps/authentication/forms.py
+++ messages/apps/authentication/forms.py
@@ -1,5 +1,26 @@
 from django.contrib.auth import get_user_model
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import (
+    UserCreationForm,
+    AuthenticationForm,
+    UsernameField,
+)
+from django import forms
+
+
+class UserLoginForm(AuthenticationForm):
+    username = UsernameField(
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Enter your username"}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                "class": "form-control",
+                "placeholder": "Enter your password",
+            }
+        )
+    )
 
 
 class SignUpForm(UserCreationForm):
--- messages/apps/authentication/urls.py
+++ messages/apps/authentication/urls.py
@@ -8,13 +8,16 @@ from django.contrib.auth.views import (
 
 
 from . import views
+from . import forms
 
 
 urlpatterns = [
     path(
         "",
         LoginView.as_view(
-            template_name="authentication/login.html", redirect_authenticated_user=True
+            template_name="authentication/login.html",
+            redirect_authenticated_user=True,
+            authentication_form=forms.UserLoginForm,
         ),
         name="login",
     ),
