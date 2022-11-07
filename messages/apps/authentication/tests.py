ea87d1a76a447347c79a54107a106c330ae341f2
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2022-11-07 14:08:25 +0100

    :white_check_mark: add signup redirection test

--- messages/apps/authentication/tests.py
+++ messages/apps/authentication/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SignUpViewTest(TestCase):
+    def test_signup_redict_to_home_page(self):
+        data = {
+            "username": "test123",
+            "first_name": "Jean",
+            "last_name": "Dujardin",
+            "password1": "aStrongPassword",
+            "password2": "aStrongPassword",
+        }
+        response = self.client.post(reverse("signup"), data)
+        self.assertRedirects(response, "/home/")
