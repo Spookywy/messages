0442cafd75760d0af4fae35a4dd4c45d29e45605
Valentin Menoret <109912731+Spookywy@users.noreply.github.com>
2022-11-26 15:01:48 +0100

    :white_check_mark: fix tests

--- messages/messages/settings.py
+++ messages/messages/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", 0)))
+DEBUG = bool(int(os.environ.get("DEBUG", 0))) if os.environ.get("DEBUG") else False
 
 ALLOWED_HOSTS = []
 ALLOWED_HOSTS.extend(filter(None, os.environ.get("ALLOWED_HOSTS", "").split(",")))
